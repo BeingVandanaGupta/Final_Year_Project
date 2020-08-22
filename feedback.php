522ed86ecb4cc63c70762ed25bf8884c58789746
BeingVandanaGupta <70052844+BeingVandanaGupta@users.noreply.github.com>
2020-08-22 20:28:26 +0530

    Add files via upload
    commit

---
+++ feedback.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.12.3, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-1.png" type="image/x-icon">
+  <meta name="description" content="Website Builder Description">
+  
+  
+  <title>Feedback form</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-rUSVEQW6AW" once="menu" id="menu1-5">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="index.html">
+                         <img src="assets/images/logo-1.png" alt="noteswala" title="" style="height: 5.1rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="index.html"><span class="mbri-arrow-next mbr-iconfont mbr-iconfont-btn"></span></a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="notes.html"><span class="mbri-edit mbr-iconfont mbr-iconfont-btn"></span>
+                        Notes
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="papers.html"><span class="mbri-file mbr-iconfont mbr-iconfont-btn"></span>
+                        Papers
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="upload.php"><span class="mbri-upload mbr-iconfont mbr-iconfont-btn"></span>
+                        upload
+                    </a>
+                </li></ul>
+        </div>
+    </nav>
+</section>
+
+<section class="mbr-section form1 cid-rWJEzrO91L" id="form1-41">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-12 col-lg-8">
+                <h2 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">
+                    FEEDBACK FORM
+                </h2>
+                <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-5">
+                    Please provide your valuable feedback to us regarding our notes and papers.
+                </h3>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="media-container-column col-lg-8" data-form-type="formoid">
+                <form action="" method="POST" class="mbr-form form-with-styler" data-form-title="Form"><input type="hidden" name="email" data-form-email="true" value="XYnwPu75mex/hzkvIlP7MYLCXCx0Ossop8LR38QzGzKMbQrC0MxFFjvQjnlSEeKe6Mi8xa4Z4RQqjx/OtaLu7bG3U012Hg3RS4lWkOvu+9K2p15UMXhlKKeQK2iF+P04">
+                    <div class="row" >
+                        <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Thanks for filling out the form!</div>
+                        <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
+                        </div>
+                    </div>
+                    <div class="dragArea row">
+                        <div class="col-md-4  form-group" data-for="name">
+                            <label for="name-form1-41" class="form-control-label mbr-fonts-style display-7">Name</label>
+                            <input type="text" name="name" data-form-field="Name" required="required" class="form-control display-7" id="name-form1-41">
+                        </div>
+                        <div class="col-md-4  form-group" data-for="email">
+                            <label for="email-form1-41" class="form-control-label mbr-fonts-style display-7">Email</label>
+                            <input type="email" name="email" data-form-field="Email" required="required" class="form-control display-7" id="email-form1-41">
+                        </div>
+                        <div data-for="phone" class="col-md-4  form-group">
+                            <label for="phone-form1-41" class="form-control-label mbr-fonts-style display-7">Phone</label>
+                            <input type="tel" name="phone" data-form-field="Phone" class="form-control display-7" id="phone-form1-41">
+                        </div>
+                        <div data-for="message" class="col-md-12 form-group">
+                            <label for="message-form1-41" class="form-control-label mbr-fonts-style display-7">Message</label>
+                            <textarea name="message" data-form-field="Message" class="form-control display-7" id="message-form1-41"></textarea>
+                        </div>
+                        <div class="col-md-12 input-group-btn align-center">
+                            <button type="submit" class="btn btn-primary btn-form display-4">SUBMIT</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+</section>
+<section class="cid-rUTqwGfzDi" id="footer1-v">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="index.html">
+                        <img src="assets/images/logo.png" alt="Mobirise" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">Badlapur Jaunpur&nbsp;<br>Uttar Pradesh, &nbsp;222175</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Contacts Us</h5>
+                <p class="mbr-text">
+                    Email: suyashtripathi98@gmail.com
+                    <br>Phone: +91 &nbsp; 8574266136&nbsp;<br>Fax: +91 &nbsp; 9889071916</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Links
+                </h5>
+                <p class="mbr-text"><a href="aboutus.html"><font color="white">About Us</font></a><br></p>
+                <p class="mbr-text"><a href="feedback.php"><font color="white">Feedback form</font></a><br></p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        © Copyright 2019 Noteswala - All Rights Reserved
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-list align-right">
+                        <div class="soc-item">
+                            <a href="https://twitter.com/tripathisuyash" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-twitter socicon"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/suyash.tripathi.5070" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
+                            </a>
+                        </div>
+                        
+                        
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+<?php
+
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])) {
+  if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message'])) {
+    echo 'All fields are required';
+  }
+  else {
+
+    $formdata = array(
+      'Name'=> $_POST['name'],
+      'email'=> $_POST['email'],
+      'Phone'=> $_POST['phone'],
+      'Message'=> $_POST['message']
+    );
+
+    $jsondata = json_encode($formdata, JSON_PRETTY_PRINT);
+
+    if(file_put_contents('feedback/formdata.txt', $jsondata,FILE_APPEND))
+    echo '<script>alert("You have successfully submitted the form")</script>';
+    else 
+    echo '<script>alert("Form is not subimtted successfully")</script>';
+  }
+}
+
+?>
+  
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-1.png" type="image/x-icon">
+  <meta name="description" content="Website Creator Description">
+  
+  
+  <title>upload</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  <style>
+      [type="file"] {
+  height: 0;
+  overflow: hidden;
+  width: 0;
+}
+
+[type="file"] + label {
+  background: transparent;
+  border: none;
+  border-radius: 5px;
+
+  cursor: pointer;
+  display: inline-block;
+  margin-bottom: 0rem;
+  outline: none;
+  padding: 0rem 0px;
+  position: relative;
+  vertical-align: middle;
+}
+  
+  </style>
+</head>
+<body>
+  <section class="menu cid-rUSVEQW6AW" once="menu" id="menu1-3w">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="index.html">
+                         <img src="assets/images/logo-1.png" alt="Mobirise" title="" style="height: 4.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="index.html"><span class="mbri-arrow-next mbr-iconfont mbr-iconfont-btn"></span></a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="notes.html"><span class="mbri-edit mbr-iconfont mbr-iconfont-btn"></span>
+                        Notes
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="papers.html"><span class="mbri-file mbr-iconfont mbr-iconfont-btn"></span>
+                        Papers
+                    </a>
+                </li></ul>
+            
+        </div>
+    </nav>
+</section>
+<section class="header3 cid-rUT4tYLnG0 mbr-fullscreen mbr-parallax-background" id="header3-4">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.2; background-color: rgb(70, 80, 82);">
+    </div>
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="media-content">
+                <h1 class="mbr-section-title mbr-white align-center mbr-bold pb-3 mbr-fonts-style display-1">Noteswala</h1>
+                <h3 class="mbr-section-subtitle mbr-white align-center mbr-light pb-3 mbr-fonts-style display-2">Thankyou</h3>
+                <div class="mbr-section-text mbr-white pb-3 ">
+                <p class="mbr-text align-center pb-3 mbr-fonts-style display-7">&nbsp;For Uploading the notes or papers<br>it would help many of students for Preparing their semester exams<br> as well as competitive exams</p></div>
+                <form action="" method="POST" enctype="multipart/form-data">
+                 <div class="mbr-section-btn align-center">
+				     <input type="file" name='file' id='file' onchange="form.submit()"/>
+				     <label for="file" class="btn-2">
+				     <span class="btn btn-md btn-primary display-4"><span class="mbr-iconfont mbri-file">Upload</span></span></label>
+                 </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</section>
+<script type="text/javascript" src="~/lib/jquery/dist/jquery.js"></script>
+
+
+
+<section class="cid-rUTqsqiqcy" id="footer1-u">
+        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+
+
+<?php
+$target_path = "uploaded/";
+
+$target_path = $target_path . basename( $_FILES['file']['name']); 
+
+if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+    echo "The file ".  basename( $_FILES['file']['name']). 
+    " has been uploaded";
+    echo '<script>alert("Unsuccessful! Please try again.");</script>';
+} else{
+    echo '<script>alert("Unsuccessful! Please try again.");</script>';
+}
+?>
+</body>
+</html>
